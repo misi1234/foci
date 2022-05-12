a2a0e5f758abaa01525dd86c80524209175a3757
misi1234 <pappmihaly1234567989@gmail.com>
2022-05-12 11:23:41 +0200

    elso feladat

--- feladat.py
+++ feladat.py
@@ -1,13 +1,26 @@
 import random
 
 
-tarolasra_alkalmas_adatszerkezet =  []
+lista_1 = []
+lista_2 = []
 
 #1
 for i in range(5):
-    tarolasra_alkalmas_adatszerkezet.append(random.randint(1,5))
+    lista_1.append(random.randint(1,5))
+
+for i in range(5):
+    lista_2.append(random.randint(1,5))
+
+print('1. feladat - Eredmények')
+
+print(f"1. forduló: {lista_1[0]}:{lista_2[0]}")
+print(f"2. forduló: {lista_1[1]}:{lista_2[1]}")
+print(f"3. forduló: {lista_1[2]}:{lista_2[2]}")
+print(f"4. forduló: {lista_1[3]}:{lista_2[3]}")
 
 
 #2
+
+
 #3
 #4
